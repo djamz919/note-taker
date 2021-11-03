ffcfca807581b5e3c391311f3d0657fc2fc20631
Daryll M <djtm97@gmail.com>
2021-11-03 18:47:32 -0400

    give express access to public folder

--- server.js
+++ server.js
@@ -12,16 +12,17 @@ const PORT = 3001;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public')); // Give express access to the public folder
 
 // Routes
 // app.use('/api', apiRoutes);
 // app.use('/', htmlRoutes);
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html/'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 // Listener
