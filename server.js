6810783d0efdb25d774632fc2e56561d4ec7bda1
Daryll M <djtm97@gmail.com>
2021-11-02 22:37:24 -0400

    update directories

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html/'));
 });
 
 // Listener
