8f2cca30c52fc34bab33dc1678a7053d72a66362
Daryll M <djtm97@gmail.com>
2021-11-06 18:14:35 -0400

    fix json

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 // const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
 const path = require('path');
-const { dbnotes } = require('./db/db.json');
+const dbnotes = require('./db/db.json');
 
 console.log(dbnotes);
 
@@ -22,12 +22,12 @@ function createNewNote(body, notesArray) {
     const note = body;
     notesArray.push(note);
     fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify({ notesArray }, null, 2));
-    return note;
+    return notesArray;
 }
 
-function updateNotesArray(newArray) {
-    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify({ newArray }, null, 2));
-    return newArray;
+function updateNotesArray(notesArray) {
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify({ notesArray }, null, 2));
+    return notesArray;
 }
 
 // Sets up the Express app to handle data parsing
@@ -50,15 +50,15 @@ app.get('/api/notes', (req, res) => {
     res.json(dbnotes);
 });
 
-app.post('api/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
     // set id based on what the next index of the array will be
-    req.body.id = animals.length.toString();
+    req.body.id = dbnotes.length.toString();
 
     const note = createNewNote(req.body, dbnotes);
     res.json(note);
 });
 
-app.delete('api/note/:id', (req,res) => {
+app.delete('/api/notes/:id', (req,res) => {
     const newArray = filterAllButId(req.body.id, dbnotes);
     res.json(newArray);
 });
