0beba1cb679362f6a9d65fc93fc0458c5df62f6a
Daryll M <djtm97@gmail.com>
2021-11-06 17:44:46 -0400

    add api calls

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 // Dependencies
 // =============================================================
 const express = require('express');
+const fs = require('fs');
 // const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
 const path = require('path');
+const { dbnotes } = require('./db/db.json');
 
+console.log(dbnotes);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+function filterAllButId(id, notesArray) {
+    const result = notesArray.filter(note => note.id !== id);
+    updateNotesArray(result);
+    return result;
+}
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify({ notesArray }, null, 2));
+    return note;
+}
+
+function updateNotesArray(newArray) {
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify({ newArray }, null, 2));
+    return newArray;
+}
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -25,8 +46,25 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+app.get('/api/notes', (req, res) => {
+    res.json(dbnotes);
+});
+
+app.post('api/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    const note = createNewNote(req.body, dbnotes);
+    res.json(note);
+});
+
+app.delete('api/note/:id', (req,res) => {
+    const newArray = filterAllButId(req.body.id, dbnotes);
+    res.json(newArray);
+});
+
 // Listener
 // =============================================================
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
-  });
+});
